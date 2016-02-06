8655772c8daa11cc58f830f3c400b7f1f32c42c4
techfreak <admin@techfreak.pl>
2016-02-06 09:04:03 +0100

    ADD STATS stats.nettemp.pl - Your nettemp send version, system, rpi model to nettemp server

---
+++ modules/tools/send_stats.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+DIR=$( cd "$( dirname "$0" )" && cd ../../ && pwd )
+
+## DATE
+DATE=$(date "+%Y%m%d%H%M%S")
+
+## RPI VER
+cd $DIR
+VER1=$(/usr/bin/git branch |grep [*]|awk '{print $2}' && awk '/Changelog/{y=1;next}y' readme.md |head -2 |grep -v '^$')
+VER=$(echo $VER1|sed 's/ /%20/g')
+
+## ID
+ETH=$(cat /sys/class/net/eth0/address)
+WLAN=$(cat /sys/class/net/wlan0/address)
+RPI=$(gpio -v |grep Type: | awk -F"," '{print $1" "$2" "$3}' |sed 's/Type://g' |sed 's/ /%20/g')
+
+if [[ -n $ETH ]]; then
+    NID=$(echo $ETH| md5sum |cut -c 1-32)
+elif [[ -n $WLAN ]]; then
+    NID=$(echo $WLAN| md5sum |cut -c 1-32)
+fi
+
+## OS
+source /etc/os-release
+#echo $PRETTY_NAME
+
+## CPUN
+CPUN=$(cat /proc/cpuinfo |grep -c processor)
+
+##UPTIME
+UPTIME=$(uptime | awk {'print $3'} |sed 's/,//g'|sed 's/:/%3A/g')
+
+## MAIN
+curl --connect-timeout 20 -G "http://stats.nettemp.pl/get.php" -d "ver=$VER&nid=$NID&rpi=$RPI&os=$ID&time=$DATE&uptime=$UPTIME&cpu=$CPUN"
